a0f1bf2211cc60c5ed30f6215ac92ab86b5d9b6e
Ralf Wigand <ralf@wigand.de>
2016-02-02 15:49:37 +0100

    initial commit

---
+++ copyblob.ps1
@@ -0,0 +1,12 @@
+﻿$sourceStorageAccountName = "storage01"
+$sourceStorageaccountKey = Get-AzureStorageKey -StorageAccountName $sourceStorageAccountName |select -ExpandProperty Primary
+$sourceStorageContext = New-AzureStorageContext -StorageAccountName $sourceStorageAccountName -StorageAccountKey $sourceStorageaccountKey
+$sourceContainer = "cont01"
+$sourceBlob = "wichtigset-azuresub.zip"
+
+$destStorageAccountName = "storage02"
+$destStorageAccountKey = Get-AzureStorageKey -StorageAccountName $destStorageAccountName |select -ExpandProperty Primary
+$destStorageContext = New-AzureStorageContext -StorageAccountName $destStorageAccountName -StorageAccountKey $destStorageAccountKey
+$destContainer = "cont02"
+
+Start-AzureStorageBlobCopy -SrcBlob $sourceBlob -SrcContainer $sourceContainer -Context $sourceStorageContext -DestContainer $destContainer -DestContext $destStorageContext
---
+++ debuginfo.ps1
@@ -0,0 +1,18 @@
+﻿$DebugPreference="Continue"
+
+write-host "Get-Host"
+Get-Host
+
+write-host "Get Azure module"
+$(Get-Module Azure).Version
+
+write-host "Get Azure Environment"
+Get-AzureEnvironment
+
+write-host "Get-AzureSubscriptions"
+Get-AzureSubscription
+
+write_host "Get sample command from Blackforest"
+Select-AzureSubscription -SubscriptionName "put your subscription name in here"
+Get-AzureLocation
+
