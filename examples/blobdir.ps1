ceb4e3566784e1f23652f05e712afe1d075cc172
Ralf Wigand <ralf@wigand.de>
2016-02-17 19:36:06 +0100

    added StorageAccountName for better cut&paste

--- examples/blobdir.ps1
+++ examples/blobdir.ps1
@@ -6,7 +6,7 @@
 
     if (-not $account) {
     #empty call, list storage accounts
-	    get-azurestorageaccount |select label,location,accounttype
+	    get-azurestorageaccount |select StorageAccountName,Label,Location,AccountType
     } elseif (-not $container){
     #account given, list containers
 	    $context=New-AzureStorageContext -StorageAccountName $account -StorageAccountKey (get-azurestoragekey -StorageAccountName $account|select -ExpandProperty Primary)
