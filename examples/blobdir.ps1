2377a534130b07e389881a8c435256ea7024c099
Ralf Wigand <ralf@wigand.de>
2016-02-17 11:49:03 +0100

    Initial, listing for blogpost Azure Blob Storage

---
+++ examples/blobdir.ps1
@@ -0,0 +1,18 @@
+﻿function blobdir {
+    param(
+    [parameter(Position=0, Mandatory=$false)][string]$account,
+    [parameter(Position=1, Mandatory=$false)][string]$container
+    )
+
+    if (-not $account) {
+    #empty call, list storage accounts
+	    get-azurestorageaccount |select label,location,accounttype
+    } elseif (-not $container){
+    #account given, list containers
+	    $context=New-AzureStorageContext -StorageAccountName $account -StorageAccountKey (get-azurestoragekey -StorageAccountName $account|select -ExpandProperty Primary)
+	    get-azurestoragecontainer -context $context
+    } else {
+	    $context=New-AzureStorageContext -StorageAccountName $account -StorageAccountKey (get-azurestoragekey -StorageAccountName $account|select -ExpandProperty Primary)
+	    get-azurestorageblob -context $context -container $container
+    }
+}
