1395f97e7cccfffa8bd657f40baa615f575c3b5e
gitralf <github@wigand.de>
2016-02-29 16:09:55 -0800

    Added files via upload

---
+++ examples/show-used-OS-disks-per-vm.ps1
@@ -0,0 +1,5 @@
+﻿get-azurevm |foreach {
+    $vm=get-azurevm -name $_.Name -servicename $_.ServiceName
+    write-host $_.name " is using " -NoNewline
+    write-host $vm.VM.OSVirtualHardDisk.MediaLink
+}
