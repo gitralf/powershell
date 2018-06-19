7febf03e67355a7288dc450d44f9071837fad8bf
Ralf Wigand <ralfwi@microsoft.com>
2018-06-19 14:15:26 -0700

    new format

--- project27/report-htm.ps1
+++ project27/report-htm.ps1
@@ -1,25 +1,34 @@
 Param(
-    [parameter(mandatory=$false)][string]$out
+    [parameter(mandatory=$false)][string]$outdir,
+    [parameter(mandatory=$false)][string]$param_resourcegroup
 )
 
 $now=(get-date -UFormat "%Y%m%d%H%M%S").ToString()
 
-if ($out.Length -eq 0){
-    $out=$env:TEMP + "\report"+$now+".htm"
+if ($outdir.Length -eq 0){
+    $outdir=$env:TEMP + "\report"+$now
 }
 
-"output goes to {0}" -f $out
+"output goes to {0}" -f $outdir
 
+if(!(Test-Path -Path $outdir )){
+    "creating directory..."
+    New-Item -ItemType directory -Path $outdir
+}
 
 $sub=Get-AzureRmSubscription
 
-$Resources=Get-AzureRmResource
+$Resources=Get-AzureRmResource 
 $nrResources=$Resources.count
 
-$Resourcegroups=Get-AzureRmResourceGroup
+if ($param_resourcegroup.length -gt 0){
+    $Resourcegroups = Get-AzureRmResourceGroup -Name $param_resourcegroup
+} else {
+    $Resourcegroups = Get-AzureRmResourceGroup
+}
 $nrResourcegroups = $Resourcegroups.Count
 
-$output = "
+$outputhead = "
 <html>
   <head>
   <style>
@@ -56,136 +65,151 @@ $output = "
   </head>
 "
 
-$output += "
+$outputmain = $outputhead + "
   <body>
-    <h1>Azure Inventory</h1>
+    <h1 id='top'>Azure Inventory</h1>
     <table id=inventory width='50%'>
-      <tr>
-        <td>
-            created:
-        </td>
-        <td>
-            {0}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Tenant-ID:
-        </td>
-        <td>
-            {1}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Subscription-ID:
-        </td>
-        <td>
-                {2}
-        </td>
-    </tr>
-    <tr>
-        <td>
-            Subscription name:
-        </td>
-        <td>
-            {3}    
-        </td>
-    </tr>
-</table>" -f $now,$sub.TenantId,$sub.SubscriptionId,$sub.Name
+        <tr><td>created:</td><td>{0}</td></tr>
+        <tr><td>Tenant-ID:</td><td>{1}</td></tr>
+        <tr><td>Subscription-ID:</td><td>{2}</td></tr>
+        <tr><td>Subscription name:</td><td>{3}</td></tr>
+    </table>" -f $now,$sub.TenantId,$sub.SubscriptionId,$sub.Name
 
-$output += "
-<hr>
-<h1>
-    Resourcegroups Overview
-</h1>
-
+$outputmain += "
+<h1>Resourcegroups</h1>
 <p>Total: {0}</p>
 
-<table id=inventory>
-    <tr>
-        <th align='left'>
-            Resourcegroupname
-        </th>
-        <th align='left'>
-            Location
-        </th>
-    </tr>
+<table id=inventory width='50%'>
+    <tr><th>Resourcegroupname</th><th>Location</th></tr>
 " -f $nrResourcegroups
 
-Get-AzureRmResourceGroup | ForEach-Object {
-    $output += "<tr><td><a href='#{0}'>{0}</a></td><td>{1}</td></tr>`n" -f $_.ResourceGroupName,$_.Location
+foreach ($resourcegroup in $Resourcegroups) {
+    $outputmain += "<tr><td><a href='{0}.htm'>{0}</a></td><td>{1}</td></tr>`n" -f $resourcegroup.ResourceGroupName,$resourcegroup.Location
 }
 
-$output +="</table>
+$outputmain +="</table>
+</body>
+</html>"
 
-<hr>
-<h1> Resourcegroups with resources </h1>
-"
+$outputmain | Out-File -filepath $outdir+"\main.htm"
 
-foreach ($resourcegroup in $Resources){
+$chapter=0;
+$section=0
+$subsection=0
+$subsubsection=0
+
+foreach ($resourcegroup in $Resourcegroups){
     $resourcegroupname=$resourcegroup.ResourceGroupName 
-    $output +="
-    <h2 id='{0}'>{0}</h2>
-    <table id=inventory width='75%'>
-    <th>Name</th><th>Type</th>
-    " -f $_.ResourceGroupName
-    $detail=""
-    $Resources | Where-Object {$_.resourcegroupname -eq $resourcegroupname}| ForEach-Object {
-        $thisresource=$_
-        $linkname=$resourcegroupname+"_"+$thisresource.Name
-        $output += "<tr><td><a href='#{0}'>{1}</td><td>{2}</td></tr>" -f $linkname,$thisresource.Name,$thisresource.resourcetype
+    $chapter++
+    $section=0
+    $output = $outputhead +"
+    <h1>{0}. Resourcegroup {1}</h2>" -f $chapter,$resourcegroupname
 
-        $detail +="<h3 id="+$linkname+">Details for "+$thisresource.Name+" ("+$resourcegroupname+")</h3>"
-$linkname
-        switch ($_.resourcetype) {
+    if ($resourcegroup.tags.keys.length -gt 0){
+        $output +"<table id=inventory width='50%'>`n<tr><th>Tag</th><th>Value</th></tr>`n"
+    }
+    foreach ($key in $resourcegroup.tags.keys){
+        $output +="<tr><td>{0}</td><td>{1}</td></tr>`n" -f $key, $resourcegroup.tags[$key]
+    }
+
+    $output+="</table>"
+    
+    $resourcetable = ""
+    $Resources | Where-Object {$_.resourcegroupname -eq $resourcegroupname}| ForEach-Object {
+        $section++
+        $thisresource=$_
+        $linkname="{0}#{1}" -f $resourcegroupname,$thisresource.Name
+
+        $resourcetable += "<tr><td><a href='{0}'>{1}</td><td>{2}</td></tr>" -f $linkname,$thisresource.Name,$thisresource.resourcetype
+        
+        $detailtable += "<h3 id={0}>{1}.{2} Resource {3} in resourcegroup {4}</h3>" -f $linkname,$chapter,$section,$thisresource.Name,$resourcegroupname
+        $detailtable += "<table id=inventory width='50%'>`n<tr><th>Attribute</th><th>Value</th></tr>`n"
+        $detailtable +="<tr><td>ResourceType</td><td>{0}</td></tr>`n" -f $thisresource.resourcetype
+        $tags=""
+        foreach ($key in $thisresource.tags.keys){
+            $tagstable+="<tr><td>Tag: {0}</td><td>{1}</td></tr>`n" -f $key, $thisresource.tags[$key]
+        }
+
+        switch ($thisresource.resourcetype) {
 
             "Microsoft.Compute/virtualMachines" {
-                $detail+="<table id=inventory>`n"
                 $vm=get-azurermvm -Name $thisresource.Resourcename -ResourceGroupName $resourcegroupname -WarningAction "SilentlyContinue"
-                $detail+="<tr><td>VM size</td><td>"+$vm.HardwareProfile.vmSize+"</td></tr>`n"
+                $detailtable+="<tr><td>VM size</td><td>{0}</td></tr>`n" -f $vm.HardwareProfile.vmSize
                 
                 if ($vm.StorageProfile.ImageReference){
-                    $detail+="<tr><td>Image offer</td><td>"+$vm.storageProfile.imagereference.offer+"</td></tr>"
-                    $detail+="<tr><td>Image SKU</td><td>"+$vm.storageProfile.ImageReference.Sku+"</td></tr>"
-                    $detail+="<tr><td>Image publisher</td><td>"+$vm.storagProfile.imageReference.publisher+"</td></tr>"
+                    $detailtable+="<tr><td>Image offer    </td><td>{0}</td></tr>" -f $vm.storageProfile.Imagereference.offer
+                    $detailtable+="<tr><td>Image SKU      </td><td>{0}</td></tr>" -f $vm.storageProfile.ImageReference.Sku
+                    $detailtable+="<tr><td>Image publisher</td><td>{0}</td></tr>" -f $vm.storageProfile.ImageReference.publisher
                 }
-                $temp=get-azurermvm -ResourceGroupName $thisresource.resourcegroupname -Name $thisresource.Resourcename -status -InformationAction "SilentlyContinue" -WarningAction "SilentlyContinue"
+                $temp=get-azurermvm -ResourceGroupName $resourcegroupname -Name $thisresource.Resourcename -status -InformationAction "SilentlyContinue" -WarningAction "SilentlyContinue"
                 ForEach ($VMStatus in $temp.Statuses){
                     if ($VMStatus.Code -like "PowerState/*"){
                         $status=$VMStatus.Code.split("/")[1]
-                        $detail+="<tr><td>PowerState</td><td>"+$status+"</td></tr>"
+                        $detailtable+="<tr><td>PowerState</td><td>{0}</td></tr>`n" -f $status
                     }
                 }
-                $detail+="</table>`n"
             }
             "Microsoft.Storage/storageAccounts" {
-                $detail+="<table id=inventory>`n"
-                $detail+="<tr><td>SKU name</td><td>"+$thisresource.sku.name+"</td></tr>"
-                $detail+="<tr><td>SKU tier</td><td>"+$thisresource.sku.tier+"</td></tr>"
-                $detail+="</table>`n"
+                $detailtable+="<tr><td>SKU name</td><td>{0}</td></tr>" -f $thisresource.sku.name
+                $detailtable+="<tr><td>SKU tier</td><td>{0}</td></tr>" -f $thisresource.sku.tier
             }
             "Microsoft.Web/sites" {
-                $detail+="<table id=inventory>`n"
-                $detail+="<tr><td>State:</td><td>"+$thisresource.Properties.state+"</td></tr>"
+                $website=Get-AzureRmWebApp -ResourceGroupName $resourcegroupname -Name $thisresource.name
+                $detailtable+="<tr><td>State:</td><td>{0}</td></tr>" -f $website.state
 
-                foreach ($hostname in $thisresource.Properties.hostNames){
-                    $detail+="<tr><td>hostname</td><td>"+$hostname+"</td></tr>"
+                foreach ($hostname in $website.hostNames){
+                    $detailtable+="<tr><td>hostname</td><td>{0}</td></tr>" -f $hostname
                 }
-                $detail+="</table>`n"
             }
-            Default {}
-        }
-    }
-    $output +="</table>"+$detail
+            "Microsoft.Sql/servers" {
+                $detailtable+="<tr><td>Kind</td><td>{0}</td></tr>" -f $thisresource.Kind
+            }
+            "Microsoft.Sql/servers/databases" {
+                $detailtable+="<tr><td>Kind</td><td>{0}</td></tr>" -f $thisresource.Kind
+            }
+            "Microsoft.Network/networkInterfaces" {
+                $nic = Get-AzureRmNetworkInterface -Name $thisresource.Name -ResourceGroupName $thisresource.resourcegroupname 
+                $linkedVMId = $nic.VirtualMachine.Id
+                if ($linkedVMId){
+                    $linkedVM = Get-AzureRmResource -ResourceId $linkedVMId
+                    $detailtable+="<tr><td>attachedTo</td><td><a href='#{0}_{1}'>{2} ({3})</a></td></tr>" -f $linkedVM.Resourcegroupname,$linkedVM.Name,$linkedVM.Name,$linkedVM.Resourcegroupname
+                }
+                $subsubsection=0
+                $extratable=""
+                foreach ($ipconfig in $nic.IpConfigurations){
+                    $subsubsection++
+                    $sublinkname= "{0}_{1}" -f $linkname,$nic.Name
+                    
+                    $extratable+="<h4 id='{0}'>{1}.{2}.{3}.{4}. IP-Configuration {5} used in {6}</h4>
+                    " -f $sublinkname,$chapter,$section,$subsection,$subsubsection,$ipconfig.name,$nic.name
+                    $extratable += "<table id=inventory width='50%'>`n<tr><th>Attribute</th><th>Value</th></tr>`n" 
+                    $extratable += "<tr><td>Name</td><td>{0}</td></tr>`n" -f $ipconfig.Name
+                    $extratable += "<tr><td>PrivateIP</td><td>{0}</td></tr>`n" -f $ipconfig.PrivateIpAddress
+                    $extratable += "<tr><td>AllocationMethodPrivateIP</td><td>{0}</td></tr>`n" -f $ipconfig.PrivateIPAllocationMethod
+#more might go in here from ipconfig
+                    $subnetID=$ipconfig.Subnet.Id
+                    $subnetparts=$subnetid.split("/")
+                    
 
+                    $extratable += "<tr><td>Subnet</td><td>{0}</td></tr>`n" -f $ipconfig.PrivateIpAddress
+                    $extratable+="</table>`n"
+                    $detailtable+="<tr><td>IPconfig</td><td><a href='#{0}'>{1}</a></td></tr>`n" -f $sublinkname,$ipconfig.name
+                }
+ 
+            }
+
+
+            Default {
+                $detailtable +="<tr><td colspan=2>no handler found</td></tr>"
+            }
+        }
+        $detailtable+=$tagstable + "</table>`n" + $extratable
+        $extratable=""
+    }
+    $output += $resourcetable + $detailtable 
+
+    $output +="</table>{0}</body></html>" -f $detailtable
+    $output | Out-File -filepath $outdir+"\"+$resourcegroupname+".htm"
 }
 
 
-
-
-$output+="
-  </body>
-</html>"
-
-$output | Out-File -filepath $out 
